5da11c18aa9c8dc38c6f054d2be3aacc55fe80a1
ediga hariprasad <ehariprasad2658@gmail.com>
2025-09-18 14:34:31 +0530

    using new issue template

---
+++ non-prod/us-west-2/dev/server/variables.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+    type = string
+    default = "ami-06a974f9b8a97ecf2"
+    description = "this is server of id"
+  
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+    description = "this is server od storage"
+  
+}
